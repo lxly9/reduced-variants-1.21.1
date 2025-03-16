daa84b3771936ab9f28b7395345b6a46523330db
Lily <lilyschlossnagel@icloud.com>
2025-03-16 17:53:45 +0100

    Initial commit

---
+++ src/client/java/com/reduced_variants/ReducedVariantsClient.java
@@ -0,0 +1,10 @@
+package com.reduced_variants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReducedVariantsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
